435b21243d862c4bf766f5ed9b259f5f98105190
BoilBoy <toua769@gmail.com>
2026-07-07 17:59:27 +0900

    feat: ログイン画面と新規登録画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sesions: 'users/sessions'
+  }
   root 'tops#home'
 end
