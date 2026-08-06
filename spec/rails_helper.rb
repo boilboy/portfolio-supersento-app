f9a208b3630d2dce4e62678af0feb6f33e54c19a
BoilBoy <toua769@gmail.com>
2026-08-06 11:08:09 +0900

    chore: テスト環境の設定 (Gem導入, Dockerfile修正, ヘルパー追加)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -67,4 +67,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+
+  config.include Devise::Test::IntegrationHelpers, type: :system
+
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400] do |options|
+      options.add_argument('--headless=new')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-dev-shm-usage')
+    end
+  end
 end
